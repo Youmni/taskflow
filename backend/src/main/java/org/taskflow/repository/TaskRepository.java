4d906f6c2c87f4b274777766c0e77c5f42694c93
Youmni <youmni.malha@gmail.com>
2024-12-05 22:45:20 +0100

    Update TaskRepository: Making it more readable

--- backend/src/main/java/org/taskflow/repository/TaskRepository.java
+++ backend/src/main/java/org/taskflow/repository/TaskRepository.java
@@ -19,24 +19,29 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
     List<Task> findByTitle(String name);
     List<Task> findByTitleAndUser(String name, User user);
     List<Task> findByTitleContainingIgnoreCase(String title);
+
     List<Task> findByDueDate(LocalDate dueDate, Sort sort);
-    List<Task> findByDueDateAndUser(LocalDate dueDate,User user, Sort sort);
+    List<Task> findByDueDateAndUser(LocalDate dueDate,User user);
     List<Task> findByDueDateBefore(LocalDate dueDate, Sort sort);
     List<Task> findByDueDateBeforeAndUser(LocalDate dueDate, User user, Sort sort);
     List<Task> findByDueDateAfter(LocalDate dueDate, Sort sort);
     List<Task> findByDueDateAfterAndUser(LocalDate dueDate, User user, Sort sort);
     List<Task> findByDueDateBetween(LocalDate from, LocalDate to, Sort sort);
+
     List<Task> findByStatus(Status status, Sort sort);
     List<Task> findByStatusIn(List<Status> statuses, Sort sort);
     List<Task> findByStatusAndUser(Status status, User user, Sort sort);
     List<Task> findByStatusInAndUser(List<Status> statuses, User user,Sort sort);
+
     List<Task> findByPriority(Priority priority, Sort sort);
     List<Task> findByPriorityIn(List<Priority> priorities, Sort sort);
     List<Task> findByPriorityAndUser(Priority priority, User user, Sort sort);
     List<Task> findByPriorityInAndUser(List<Priority> priorities, User user, Sort sort);
+
     List<Task> findByCreatedAtBetween(LocalDateTime from, LocalDateTime to, Sort sort);
     List<Task> findByCreatedAtAfter(LocalDateTime createdAt, Sort sort);
     List<Task> findByCreatedAtBefore(LocalDateTime createdAt, Sort sort);
+
     List<Task> findByUser (User user, Sort sort);
 
 
